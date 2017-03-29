ec3b5cf7abc6d59d34885fc22939c2b8a0d0016a
Riccardo Cavallari <riccardocavallari@gmail.com>
2017-03-29 13:05:54 +0200

    added some stuff after reading https://nodesource.com/blog/understanding-socketio/

--- index.js
+++ index.js
@@ -6,17 +6,35 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected, id: ' + socket.id);
+// The main idea behind Socket.IO is that you can send and receive any events you want, 
+// with any data you want. Any objects that can be encoded as JSON will do, and binary 
+// data is supported too.
+
+var handleClient = function (socket) {
+    // we've got a client connection
+    console.log('a user connected, id: ' + socket.id);
+    
+    // to make things interesting, send stuff every second
+    var i = 0;
+    var interval = setInterval(function () {
+        var tweet = {user: "nodesource", text: "Hello, world! " + i};
+        socket.emit("tweet", tweet);
+        console.log("tweet %s", i);
+        i = i+1;
+    }, 1000);
+    
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+        clearInterval(interval);
+    });
   
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-  
-  socket.on('chat_message', function(msg){
-    console.log('message: ' + msg);
-  });
-});
+    socket.on('chat_message', function(msg){
+        io.emit('chat_message', msg);
+        console.log('message: ' + msg);
+    });
+};
+
+io.on('connection', handleClient);
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
