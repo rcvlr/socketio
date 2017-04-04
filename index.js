1de107cc062ba4a7a51cfa055c2c9defbecf2165
Riccardo Cavallari <riccardocavallari@gmail.com>
2017-04-04 21:40:08 +0200

    web-bluetooth - first attempt

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 // http://stackoverflow.com/questions/10058226/send-response-to-all-clients-except-sender-socket-io
 
-var express = require('express')
-var app = express()
-var http = require('http').Server(app)
-var io = require('socket.io')(http)
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 // configuration
-app.use(express.static('public'))
-app.set('port', (process.env.PORT || 5000))
+app.use(express.static('public'));
+app.set('port', (process.env.PORT || 5000));
 
 // route
 app.get('/', function(req, res){
-  res.sendFile('index.html')
+  res.sendFile('index.html');
 })
 
 var handleClient = function(socket) {
@@ -20,17 +20,17 @@ var handleClient = function(socket) {
     
     socket.on('disconnect', function(){
         console.log('user disconnected');
-    })
+    });
     
     socket.on('signal_message', function(msg){
         // broadcast to all the connected clients except the sender
-        socket.broadcast.emit('signal_message', msg)
-        console.log('signal_message: ' + msg)
-    })
+        socket.broadcast.emit('signal_message', msg);
+        console.log('signal_message: ' + msg);
+    });
 };
 
-io.on('connection', handleClient)
+io.on('connection', handleClient);
 
 http.listen(app.get('port'), function(){
-  console.log('listening on *:' + app.get('port'))
-})
+  console.log('listening on *:' + app.get('port'));
+});
--- public/js/main.js
+++ public/js/main.js
@@ -1,50 +1,104 @@
 var socket = require('socket.io-client')();
-var $ = require('jquery')
-var Peer = require('simple-peer')
-var p = new Peer({ initiator: location.hash === '#1', trickle: false })
+var $ = require('jquery');
+var Peer = require('simple-peer');
+var p = new Peer({ initiator: location.hash === '#1', trickle: false });
 
 $('#sendForm').submit(function(event){
-    event.preventDefault()
-    p.send($('#m').val())
-    $('#messages').append($('<li>').text($('#m').val()))
-    $('#m').val('')    // clear the text box
-})
+    event.preventDefault();
+    p.send($('#m').val());
+    $('#messages').append($('<li>').text($('#m').val()));
+    $('#m').val('');    // clear the text box
+});
 
 $("#discButton").click(function disconnect() {
-    socket.close()
-    p.destroy()
-    console.log("websocket and p2p disconnected")
-})
+    socket.close();
+    p.destroy();
+    console.log("websocket and p2p disconnected");
+});
 
+// ----------------------------------------------------------------------------
+// socketio event
+// ----------------------------------------------------------------------------
 socket.on('signal_message', function(msg){
-    console.log('signal message received from the signaling server')
-    p.signal(JSON.parse(msg))
-})
+    console.log('signal message received from the signaling server');
+    p.signal(JSON.parse(msg));
+});
     
 // The global socket variable is an EventEmitter-like object.
 // We can attach a listener to fire when we've connected to the server like so:
 socket.on("connect", function () {
-    console.log("websocket connected!")
-})
+    console.log("websocket connected!");
+});
 
-// simple-peer
-p.on('error', function (err) { console.log('error', err) })
+// ----------------------------------------------------------------------------
+// simple-peer event
+// ----------------------------------------------------------------------------
+p.on('error', function (err) { console.log('error', err) });
 
 p.on('signal', function (data) {
-  console.log('SIGNAL', JSON.stringify(data))
+  console.log('SIGNAL', JSON.stringify(data));
   // pass it to the signaling server
-  socket.emit('signal_message', JSON.stringify(data))
-})
+  socket.emit('signal_message', JSON.stringify(data));
+});
 
 p.on('connect', function() {
-  console.log('CONNECT')
-})
+  console.log('CONNECT');
+});
 
 p.on('close', function() {
-    console.log('CLOSE')
-})
+  console.log('CLOSE');
+});
 
 p.on('data', function(data) {
-  console.log('data: ' + data)
-  $('#messages').append($('<li>').text(data))
-})
+  console.log('data: ' + data);
+  $('#messages').append($('<li>').text(data));
+  
+  if (data == 'getBattery') {
+    if (isWebBluetoothEnabled()) {
+      getBatteryService();
+    }  
+  }
+});
+
+// ----------------------------------------------------------------------------
+// web bluetooth
+// ----------------------------------------------------------------------------
+
+function getBatteryService() {
+  log('Requesting Bluetooth Device...');
+  navigator.bluetooth.requestDevice(
+    {filters: [{services: ['battery_service']}]})
+  .then(device => {
+    log('Connecting to GATT Server...');
+    return device.gatt.connect();
+  })
+  .then(server => {
+    log('Getting Battery Service...');
+    return server.getPrimaryService('battery_service');
+  })
+  .then(service => {
+    log('Getting Battery Level Characteristic...');
+    return service.getCharacteristic('battery_level');
+  })
+  .then(characteristic => {
+    log('Reading Battery Level...');
+    return characteristic.readValue();
+  })
+  .then(value => {
+    let batteryLevel = value.getUint8(0);
+    log('> Battery Level is ' + batteryLevel + '%');
+  })
+  .catch(error => {
+    log('Argh! ' + error);
+  });
+}
+
+function isWebBluetoothEnabled() {
+  if (navigator.bluetooth) {
+    return true;
+  } else {
+    console.log('Web Bluetooth API is not available.\n' +
+                'Please make sure the "Experimental Web Platform features" flag is enabled.');
+    return false;
+  }
+}
