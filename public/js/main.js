4b1747dc65d2941927e7945c869dfc09ea251fab
Riccardo Cavallari <riccardocavallari@gmail.com>
2017-04-19 19:33:10 +0200

    getBattery now works. Tested with BLE Peripheral sim app

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,18 @@
+// To run it on a VM use the following command:
+// chromium 10.0.2.2:5000 --enable-experimental-web-platform-features --user-data-dir=/tmp/foo --unsafely-treat-insecure-origin-as-secure=http://10.0.2.2:5000/
+
 var socket = require('socket.io-client')();
 var $ = require('jquery');
 var Peer = require('simple-peer');
 var p = new Peer({ initiator: location.hash === '#1', trickle: false });
 
+$("#scanButton").hide();
+$("#scanButton").click(getBatteryService);
+
 $('#sendForm').submit(function(event){
     event.preventDefault();
     p.send($('#m').val());
-    $('#messages').append($('<li>').text($('#m').val()));
+    appendMessage($('#m').val());
     $('#m').val('');    // clear the text box
 });
 
@@ -15,6 +21,10 @@ $("#discButton").click(function disconnect() {
     p.destroy();
     console.log("websocket and p2p disconnected");
 });
+
+function appendMessage(msg) {
+  $('#messages').append($('<li>').text(msg));
+}
 
 // ----------------------------------------------------------------------------
 // socketio event
@@ -51,11 +61,13 @@ p.on('close', function() {
 
 p.on('data', function(data) {
   console.log('data: ' + data);
-  $('#messages').append($('<li>').text(data));
+  appendMessage(data);
   
   if (data == 'getBattery') {
     if (isWebBluetoothEnabled()) {
-      getBatteryService();
+      // As a security feature, discovering Bluetooth devices with 
+      // navigator.bluetooth.requestDevice must be triggered by a user gesture
+      $("#scanButton").show("slow");
     }
   }
 });
@@ -87,6 +99,8 @@ function getBatteryService() {
   .then(value => {
     let batteryLevel = value.getUint8(0);
     console.log('> Battery Level is ' + batteryLevel + '%');
+    appendMessage('> Battery Level is ' + batteryLevel + '%');
+    p.send('> Battery Level is ' + batteryLevel + '%');
   })
   .catch(error => {
     console.log('Argh! ' + error);
